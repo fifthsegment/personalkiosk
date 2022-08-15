c0db0f0f5ef2188e570569fd65c8d733cca6300d
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-08-15 18:48:33 +0000

    Update domain.tsx

--- src/components/dashboard/domain/domain.tsx
+++ src/components/dashboard/domain/domain.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 interface Props {
     children: React.ReactElement
 }
+
+// eslint test
 export const Domain = ({children}: Props) => {
     const ctx = React.createContext({});
     return <div>    
