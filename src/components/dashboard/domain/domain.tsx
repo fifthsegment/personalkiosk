9fa3fef1c3101472411be9d2f9c1d5b5e21cc7a2
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-08-15 18:44:02 +0000

    Update domain.tsx

--- src/components/dashboard/domain/domain.tsx
+++ src/components/dashboard/domain/domain.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 interface Props {
     children: React.ReactElement
 }
-
 export const Domain = ({children}: Props) => {
-    return <div>
+    const ctx = React.createContext(false);
+    return <div>    
         {children}
         </div>
 }
