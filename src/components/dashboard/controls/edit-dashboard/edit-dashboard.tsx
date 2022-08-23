e73398e4125045f234cb51c73bccf72add86367d
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-08-23 19:01:39 +0000

    Update edit-dashboard.tsx

--- src/components/dashboard/controls/edit-dashboard/edit-dashboard.tsx
+++ src/components/dashboard/controls/edit-dashboard/edit-dashboard.tsx
@@ -1,13 +1,11 @@
-import React, { SyntheticEvent, useContext } from "react";
+import { SyntheticEvent, useContext } from "react";
 import { NavLink } from "../../../../styled-components";
 import { DomainContext } from "../../domain/domain";
 import { toggleDashboardMode as externalActionToggleDashboardMode } from "../../../../actions";
-import { ApplicationContext } from "../../../../contexts/ApplicationContext";
-import { ArrowDownIcon, CheckCircleIcon, PencilIcon } from "@heroicons/react/outline";
+import { CheckCircleIcon, PencilIcon } from "@heroicons/react/outline";
 
 export const EditDashboard = () => {
   const { domain, updateDomain } = useContext(DomainContext);
-  const { data, update } = useContext(ApplicationContext);
 
   const { mode } = domain;
   const toggleDashboardMode = (e: SyntheticEvent) => {
