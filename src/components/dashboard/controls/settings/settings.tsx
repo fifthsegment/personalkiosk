3bb68799adcf8636605361cbf86d1342174209e9
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-08-26 21:41:57 +0000

    Update settings.tsx

--- src/components/dashboard/controls/settings/settings.tsx
+++ src/components/dashboard/controls/settings/settings.tsx
@@ -33,7 +33,7 @@ export const Settings = () => {
       <div>Display a color changer here</div>
       <div>
         <Formik
-          initialValues={{ name: "", email: "" }}
+          initialValues={{ color: "" }}
           onSubmit={async (values) => {
             await new Promise((resolve) => setTimeout(resolve, 500));
             alert(JSON.stringify(values, null, 2));
@@ -44,7 +44,7 @@ export const Settings = () => {
               Color
             </label>
             <Field
-              name="name"
+              name="color"
               type="text"
               className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
             />
