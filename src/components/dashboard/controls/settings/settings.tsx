e4e74a7eabf7d45437f5b1f4cfdcb6908276f160
Abdullah Irfan <abdullah.irfan1@gmail.com>
2022-09-06 20:28:04 +0000

    Update settings.tsx

--- src/components/dashboard/controls/settings/settings.tsx
+++ src/components/dashboard/controls/settings/settings.tsx
@@ -8,7 +8,6 @@ import { Form, Field, Formik } from "formik";
 import { ModalFooter } from "../../../../styled-components/modal";
 export const Settings = () => {
   const { data: AppData, update } = useContext(ApplicationContext);
-
   const [showAlert, setShowAlert] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   if (openModal === false) {
@@ -22,7 +21,7 @@ export const Settings = () => {
           setOpenModal(true);
           setTimeout(() => {
             setShowAlert(false);
-          }, 1000);
+          }, 2000);
         }}
       />
     );
@@ -57,16 +56,14 @@ export const Settings = () => {
               label="Background URL"
               component={Input}
             />
-            {showAlert && (
+            {showAlert === true ? (
               <Alert closeBtn={false} type="success">
                 <>
-                  <strong className="font-bold">Holy smokes!</strong>
-                  <span className="block sm:inline">
-                    Something seriously bad happened.
-                  </span>
+                  <strong className="font-bold">Success!</strong>
+                  <span className="block sm:inline">Settings were saved.</span>
                 </>
               </Alert>
-            )}
+            ) : null}
             <ModalFooter>
               <Button>Save</Button>
             </ModalFooter>
